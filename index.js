c405e8291f64b2af76f3bbe29a5656d7fe7effda
Ganesh Kumar <mrganeshofficial8@gmail.com>
2024-07-06 18:11:58 +0530

    render deploying issue fixed

--- index.js
+++ index.js
@@ -9,21 +9,19 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-app.use("/users",userRouter)
-app.use("/notes",noteRouter)
+app.use("/users", userRouter);
+app.use("/notes", noteRouter);
 
+app.get("/", (req, res) => {
+  res.send("This is the home page");
+});
 
-app.get("/",(req,res)=>{
-    res.send("This is the home page")
-})
-
-
-app.listen(7700,async()=>{
-    try {
-        await connection;
-        console.log("Connected to the DB");
-        console.log("server is running");
-    } catch (error) {
-        console.log(error);
-    }
-})
+app.listen(7700, async () => {
+  try {
+    await connection;
+    console.log("Connected to the DB");
+    console.log("server is running");
+  } catch (error) {
+    console.log(error);
+  }
+});
